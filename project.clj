3ec9fd5aa5050c7438dda4c9d9a1f1825144eac4
Alexander Oloo <alekcz@gmail.com>
2020-04-22 18:37:04 +0200

    testing 1 2

--- project.clj
+++ project.clj
@@ -3,13 +3,12 @@
   :url "https://github.com/alekcz/konserve-fire"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  ;:aot :all
+  :aot :all
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [io.replikativ/konserve "0.5.1"]
                  [byte-streams "0.2.2"]
                  [alekcz/fire "0.0.3-SNAPSHOT"]
-                 [io.replikativ/incognito "0.2.5"]
-                 [io.replikativ/superv.async "0.2.9"]]
+                 [io.replikativ/incognito "0.2.5"]]
   :repl-options {:init-ns konserve-fire.core}
   :plugins [[lein-cloverage "1.1.2"]]
   :profiles { :dev {:dependencies [[metosin/malli "0.0.1-SNAPSHOT"]]}})
