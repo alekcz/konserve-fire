c875909bf2d484d3201a64bc377af737aa8524c8
Alex Oloo <alekcz@gmail.com>
2021-05-18 09:47:48 +0200

    fire v0.3.2

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject alekcz/konserve-fire "0.3.0-alpha2"
+(defproject alekcz/konserve-fire "0.3.0-alpha3"
   :description "A Firebase backend for konserve."
   :url "https://github.com/alekcz/konserve-fire"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :aot :all
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [alekcz/fire "0.3.1"]
+                 [alekcz/fire "0.3.2"]
                  [io.replikativ/konserve "0.6.0-alpha3"]]
   :repl-options {:init-ns konserve-fire.core}
   :plugins [[lein-cloverage "1.2.0"]]
